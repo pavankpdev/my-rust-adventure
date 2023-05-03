1c37dae97d232011e0cc3267f143e96837aed9e5
pavanleton <pavankumarkpkp@gmail.com>
2023-05-03 21:10:44 +0530

    hello world with cargo

---
+++ hello_world_with_cargo/target/debug/deps/hello_world_with_cargo-fd0c34dd9ad2d705.d
@@ -0,0 +1,5 @@
+/Users/pavankumar/Documents/PavanKumarWork/rust/hello_world_with_cargo/target/debug/deps/hello_world_with_cargo-fd0c34dd9ad2d705: src/main.rs
+
+/Users/pavankumar/Documents/PavanKumarWork/rust/hello_world_with_cargo/target/debug/deps/hello_world_with_cargo-fd0c34dd9ad2d705.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world_with_cargo/target/debug/hello_world_with_cargo.d
@@ -0,0 +1,1 @@
+/Users/pavankumar/Documents/PavanKumarWork/rust/hello_world_with_cargo/target/debug/hello_world_with_cargo: /Users/pavankumar/Documents/PavanKumarWork/rust/hello_world_with_cargo/src/main.rs
