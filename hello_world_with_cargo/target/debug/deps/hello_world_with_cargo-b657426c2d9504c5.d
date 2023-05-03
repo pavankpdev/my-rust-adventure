3fe6cb4d041cdc3642929be03b268791c08de324
pavanleton <pavankumarkpkp@gmail.com>
2023-05-03 21:11:07 +0530

    rm: target dir

--- hello_world_with_cargo/target/debug/deps/hello_world_with_cargo-b657426c2d9504c5.d
+++
@@ -1,5 +0,0 @@
-/Users/pavankumar/Documents/PavanKumarWork/rust/hello_world_with_cargo/target/debug/deps/hello_world_with_cargo-b657426c2d9504c5.rmeta: src/main.rs
-
-/Users/pavankumar/Documents/PavanKumarWork/rust/hello_world_with_cargo/target/debug/deps/hello_world_with_cargo-b657426c2d9504c5.d: src/main.rs
-
-src/main.rs:
--- hello_world_with_cargo/target/debug/hello_world_with_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/pavankumar/Documents/PavanKumarWork/rust/hello_world_with_cargo/target/debug/hello_world_with_cargo: /Users/pavankumar/Documents/PavanKumarWork/rust/hello_world_with_cargo/src/main.rs
