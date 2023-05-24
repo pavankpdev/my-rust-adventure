900c4a5416053c2ade2428586eebbf80e3c71541
pavanleton <pavankumarkpkp@gmail.com>
2023-05-24 23:53:51 +0530

    use default in match

--- enums_/src/main.rs
+++ enums_/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
 fn plu_one(x: Option<i32>) -> Option<i32> {
     match x {
-        None => None,
-        Some(i) => Some(i + 1) // Should wrap it in Some, coz of return type in Option
+        Some(i) => Some(i + 1), // Should wrap it in Some, coz of return type in Option
+        _ => None // anything else execute this
     }
 }
